ea2b7f380f059e434920ad30f96a78cecb51317d
Abhay Joshi <abhay29082003@gmail.com>
2023-01-27 22:54:39 +0530

    Initial Commit

---
+++ include/Core.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include<iostream>
+#include<SDL.h>
+
+#include"Timer.h"
+
+class Core
+{
+public:
+	Core();
+	~Core();
+
+	void Loop();
+	void Update();
+	void Render();
+
+private:
+
+	SDL_Window* m_window;
+	SDL_Renderer* m_renderer;
+
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 600;
+
+	const int SCREEN_FPS = 60;
+	const int SCREEN_TICKS_PER_FRAME = 1000 / SCREEN_FPS;
+
+	bool m_quit;
+
+	SDL_Event m_event;
+
+	Timer m_fps_timer;
+	Timer m_cap_timer;
+
+	int m_counted_frames;
+};
---
+++ src/Core.cpp
@@ -0,0 +1,61 @@
+#include"Core.h"
+
+Core::Core()
+	:m_quit(false), m_window(NULL), m_renderer(NULL), m_counted_frames(0)
+{
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
+
+	m_window = SDL_CreateWindow("Ball Simulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+	if (m_window == NULL)
+		std::cout << "Window could not be created! SDL Error: " << SDL_GetError() << std::endl;
+
+	m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED);
+
+	if (m_renderer == NULL)
+		std::cout << "Renderer could not be created! SDL Error: " << SDL_GetError() << std::endl;
+
+	m_fps_timer.Start();
+}
+
+Core::~Core()
+{
+	SDL_DestroyRenderer(m_renderer);
+	SDL_DestroyWindow(m_window);
+
+	SDL_Quit();
+}
+
+void Core::Loop()
+{
+	while (!m_quit)
+	{
+		m_cap_timer.Start();
+
+		while (SDL_PollEvent(&m_event) != 0)
+		{
+			if (m_event.type == SDL_QUIT)
+				m_quit = true;
+		}
+
+		Update();
+		Render();
+
+		++m_counted_frames;
+
+		int frames_ticks = m_cap_timer.GetTicks();
+		if (frames_ticks < SCREEN_TICKS_PER_FRAME)
+			SDL_Delay(SCREEN_TICKS_PER_FRAME - frames_ticks);
+	}
+}
+
+void Core::Update()
+{
+	
+}
+
+void Core::Render()
+{
+
+}
